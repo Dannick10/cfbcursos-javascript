8da3e493ac4e5896448aa2434f998dbc8784928c
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-11-17 20:38:20 -0300

    funçoes anonimas

---
+++ aula 08/funçoes anonimas copy.js
@@ -0,0 +1,6 @@
+const f=new Function("v1","v2","return v1+v2") // funçao contrutor anonima
+    
+ 
+
+
+console.log(f(10,5))
---
+++ aula 08/funçoes anonimas.js
@@ -0,0 +1,13 @@
+const f=new function(...valores){
+    let res=0 
+    for(v of valores){
+        res+=v
+      
+    }
+    return res
+}
+    
+ 
+
+
+console.log(f(10,5))
