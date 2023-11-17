45330aeb1cb2140cbb27d12ae46d72fef40a0b1c
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-11-17 18:34:58 -0300

    funçao com retorno

---
+++ aula 05/funcoesretorno.js
@@ -0,0 +1,19 @@
+function canal(){
+    let n1=5
+    let n2=5 
+    let res= n1+n2
+    if(res%2 == 0)
+    return "par"
+    else 
+    return "impa"
+/* quando o return se encerra acaba a função
+*/
+     console.log('fim')
+}
+
+let num=canal()
+
+console.log(canal())
+console.log(canal() + canal())
+console.log(canal( ) + num )
+console.log(num)
