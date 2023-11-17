3c72ad57d42c19f602b3e46d744872ca954cc142
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-11-17 20:13:56 -0300

    rest

---
+++ aula 07/funçao rest2.js
@@ -0,0 +1,13 @@
+function showList(empresa, ...clientes){
+    console.log(empresa);
+    console.log(clientes);
+    console.log(clientes.length)
+}
+
+showList("Origamid","joao","Maria","ronaldo")
+
+
+const numeros = [1, 2, 3, 4, 5, 6]
+
+console.log(Math.max(...numeros))
+
