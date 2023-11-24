5fbfffd0000d93587cd5a89053d2ec0fae000745
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-11-24 11:52:01 -0300

    praticando

--- aula 20/addeventlistinner.js
+++ aula 20/addeventlistinner.js
@@ -1,27 +1,25 @@
 const c1 = document.querySelector('#caixa1')
 const c2 = document.querySelector('#caixa2')
-const btn = document.querySelector('#btn_copiar')
+const btn = document.querySelector('#btn_copy')
 
 const curso = [...document.querySelectorAll('.curso')]
 
 curso.map((el)=>{
-  el.addEventListener('click',(evt)=>{
-    const cursos = evt.target 
-    cursos.classList.toggle('destaque')
-  })
+ el.addEventListener('click',(evt)=>{
+  const cursos = evt.target
+  cursos.classList.toggle('color')
+ })
 })
 
 btn.addEventListener('click',()=>{
-    const cursoSelecionados = [...document.querySelectorAll('.destaque')]
-    const cursoNaoSelecionados = [...document.querySelectorAll('.curso:not(.destaque)')]
+  const cursoSelecionados = [...document.querySelectorAll('.color')]
+  const cursoNaoSelecionados = [...document.querySelectorAll('.curso:not(.color)')]
 
-    cursoNaoSelecionados.reverse()
-    
-    cursoSelecionados.map((el)=>{
-        c2.appendChild(el)
-    })
+  cursoSelecionados.map((el)=>{
+    c2.append(el)
+  })
 
-    cursoNaoSelecionados.map((el)=>{
-        c1.appendChild(el)
-    })
+  cursoNaoSelecionados.map((el)=>{
+    c1.append(el)
+  })
 })
