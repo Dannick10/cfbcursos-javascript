b08665572a20aa39484e5877f7af6fb2c8be8026
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-11-24 19:51:31 -0300

    Elementos do Doom

---
+++ aula 22/ElementosDOom.js
@@ -0,0 +1,9 @@
+const caixa1=document.querySelector('#caixa1')
+const curso=[...document.querySelectorAll('.curso')]
+
+console.log(caixa1.getRootNode())
+console.log(caixa1.ownerDocument)
+
+//console.log(caixa1.firstElementChild)
+//console.log(caixa1.lastElementChild)
+//console.log(caixa1.children)
