c2d5c2b91d51bdfc40187fed427c849834de4869
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-11-27 13:59:24 -0300

    removeCHild

--- aula 24/removechild.js
+++ aula 24/removechild.js
@@ -7,7 +7,17 @@ cursos.map((el,chave)=>{
     novoElemento.setAttribute('id','c' + chave + 1)
     novoElemento.setAttribute('class','curso c2')
     novoElemento.innerHTML=el
-    
+
+    const btn_lixeira=document.createElement('img')
+    btn_lixeira.setAttribute('src','54324.png')
+    btn_lixeira.setAttribute('class','btn_lixeira')
+
+    btn_lixeira.addEventListener('click',(evt)=>{
+        caixa1.removeChild(evt.target.parentNode)
+    })
+
+
+    novoElemento.appendChild(btn_lixeira)
     caixa1.appendChild(novoElemento)
 })
 
