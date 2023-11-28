a7020c6ed51c7c12f174165e25d59eb284f17778
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-11-28 12:32:49 -0300

    insert before

--- aula 28/filter.js
+++ aula 28/filter.js
@@ -1,21 +1,15 @@
 const caixa1=document.querySelector('#Caixacurso')
 const curso=[...document.querySelectorAll('.curso')]
 const cursos=['html','css','java script','bootstrap','typescript','node js',' react Native',]
-const nomeCurso= document.querySelector('#nomedocurso')
 
 const btn_cursoselecionado = document.querySelector('#btnSelecionarCurso')
 const btn_cursoremovido = document.querySelector('#btnRemoverCurso')
-const btn_cursoAdicionarAntes = document.querySelector('#btnAdicionarNovoCursoAntes')
-const btn_cursoAdicionarDepois = document.querySelector('#btnAdicionarNovoCursoDepois')
 
-let indice = 0
 
-const criarNovoCurso=(curso)=>{
-
+cursos.map((el,chave)=>{ 
     const novoElemento=document.createElement('div')
     novoElemento.setAttribute('class','curso')
-    novoElemento.setAttribute('id','c' + indice)
-    novoElemento.innerHTML=curso
+    novoElemento.innerHTML=el
 
     const comados= document.createElement('div')
     comados.setAttribute('class','comados')
@@ -23,18 +17,11 @@ const criarNovoCurso=(curso)=>{
     const rb=document.createElement('input')
     rb.setAttribute('type','radio')
     rb.setAttribute('name','rb_curso')
-    
+
     comados.appendChild(rb)
-
     novoElemento.appendChild(comados)
-    return novoElemento
-}
-
-cursos.map((el,chave)=>{ 
-
-    const novoElemento = criarNovoCurso(el)   
     caixa1.appendChild(novoElemento)
-    indice++
+
 })
 
 const radiosSelecionados=()=>{
@@ -48,11 +35,11 @@ const radiosSelecionados=()=>{
 btn_cursoselecionado.addEventListener('click',(evt)=>{
     
     const rs = radiosSelecionados()
-    try{
+    if(rs != undefined){
         const cursoSelecionado = rs.parentNode.previousSibling.textContent
         console.log(cursoSelecionado)
         window.alert(`O curso selecionado: ${cursoSelecionado}`)
-    } catch(ex){
+    } else {
         window.alert('EScolha uma opção')
     }
     //const cursoSelecionado = radiosSelecionados.parentNode.parentNode.firstChild.textContent
@@ -72,33 +59,7 @@ btn_cursoremovido.addEventListener('click',(evt)=>{
 
 })
 
-btn_cursoAdicionarAntes.addEventListener('click',()=>{
-        if(nomeCurso.value != ''){
-        const rs=radiosSelecionados()
-        const cursoSelecionado = rs.parentNode.parentNode
-        console.log(cursoSelecionado)
-        const Novocurso = criarNovoCurso(nomeCurso.value)
-        caixa1.insertBefore(Novocurso,cursoSelecionado)
-        } else {
-            window.alert('Digite o nome do curso')
-        }
-  
-  
-})
 
-btn_cursoAdicionarDepois.addEventListener('click',()=>{
-        if(nomeCurso.value != ''){
-        const rs=radiosSelecionados()
-        const cursoSelecionado = rs.parentNode.parentNode
-        console.log(cursoSelecionado)
-        const Novocurso = criarNovoCurso(nomeCurso.value)
-        caixa1.insertBefore(Novocurso,cursoSelecionado.nextSibling)
-        } else {
-            window.alert('Digite o nome do curso')
-        }
-  
-
-})
 
 // parentNode
 //childNodes [nodenumber]
