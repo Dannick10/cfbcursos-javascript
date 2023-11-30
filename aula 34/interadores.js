4bbf301a1bba0726cab6160faae824ce31532b45
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-11-30 15:18:08 -0300

    interator

---
+++ aula 34/interadores.js
@@ -0,0 +1,21 @@
+const valores =[10,8,9,2]
+const it_valores=valores[Symbol.iterator]()
+
+const text ="youtube"
+const it_text=text[Symbol.iterator]()
+
+console.log(text)
+console.log(it_text.next())
+console.log(it_text.next())
+console.log(it_text.next())
+console.log(it_text.next())
+console.log(it_text.next())
+console.log(it_text.next())
+console.log(it_text.next())
+console.log(it_text.next())
+
+
+
+//strings
+//map 
+//set
