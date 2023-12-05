63300627300dd240f52867fa6e08407d995ac264
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-12-05 19:08:25 -0300

    objetos

---
+++ aula 42/script.js
@@ -0,0 +1,47 @@
+const nome = document.querySelector('#nome')
+const idade = document.querySelector('#idade')
+const btn_enviar = document.querySelector('#btn_enviar')
+const res = document.querySelector('#res')
+
+
+class Pessoa{
+    constructor(pnome,pidade){
+        this.nome = pnome 
+        this.idade = pidade 
+    }
+
+    getNome(){
+        return this.nome 
+    }
+
+    getIdade(){
+       return this.idade 
+    }
+
+
+}
+
+
+let cadastro = []
+
+const adicionarpessoas=()=>{
+    res.innerHTML = ''
+   cadastro.map((p)=>{
+    const div = document.createElement('div')
+    div.setAttribute('class','pessoa')
+    div.innerHTML = `Nome: ${p.getNome()}<br> idade ${p.getIdade()}`
+    res.appendChild(div)
+    console.log(p)
+   })
+}
+
+btn_enviar.addEventListener('click',()=>{
+    const p = new Pessoa(nome.value,idade.value)
+    cadastro.push(p)
+    nome.value = ''
+    idade.value = ''
+    console.log(cadastro)
+    nome.focus()
+    adicionarpessoas()
+    
+})
