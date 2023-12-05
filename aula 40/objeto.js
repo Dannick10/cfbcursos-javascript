c986d457f7b59571d1288087f7045ce9f6b5f533
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-12-05 11:51:18 -0300

    objeto

---
+++ aula 40/objeto.js
@@ -0,0 +1,14 @@
+class pessoa{
+    constructor(pnome,idade){
+        this.nome=pnome
+        this.idade=15
+    }
+}
+
+let p1 = new pessoa('Daniel',15)
+let p2 = new pessoa(10)
+let p3 = new pessoa('Silva')
+
+console.log(p1)
+console.log(p2.nome)
+console.log(p3.nome)
