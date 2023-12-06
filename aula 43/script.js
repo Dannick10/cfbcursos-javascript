29d35d0cdd5fe02ab97564a102e0c2874766303f
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-12-06 13:58:45 -0300

    objetos

--- aula 43/script.js
+++ aula 43/script.js
@@ -1,45 +1,16 @@
-const nome = document.querySelector('#nome')
-const idade = document.querySelector('#idade')
-const btn_enviar = document.querySelector('#btn_enviar')
-const res = document.querySelector('#res')
-
-
-function Pessoa(pnome,pidade){
-    this.nome=pnome,
-    this.idade=pidade,
-
-    this.getNome=function(){
-        return this.nome 
-    }
-
-    this.getIdade=function(){
-       return this.idade 
-    }
-
+const Pessoas={
+    nome: 'daniel',
+    getNome: function(){
+        return this.nome
+    },
+    setNome:function(nome){
+        this.nome=nome
+    },
 
 }
 
+const p2=Pessoas
 
-let cadastro = []
 
-const adicionarpessoas=()=>{
-    res.innerHTML = ''
-   cadastro.map((p)=>{
-    const div = document.createElement('div')
-    div.setAttribute('class','pessoa')
-    div.innerHTML = `Nome: ${p.getNome()}<br> idade ${p.getIdade()}`
-    res.appendChild(div)
-    console.log(p)
-   })
-}
-
-btn_enviar.addEventListener('click',()=>{
-    const p = new Pessoa(nome.value,idade.value)
-    cadastro.push(p)
-    nome.value = ''
-    idade.value = ''
-    console.log(cadastro)
-    nome.focus()
-    adicionarpessoas()
-    
-})
+console.log(Pessoas.nome)
+console.log(p2.getNome())
