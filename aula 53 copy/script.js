e3bf60e784bd73a48cb581acf1adc0f172f7c068
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-12-12 16:07:46 -0300

    promisse

---
+++ aula 53 copy/script.js
@@ -0,0 +1,33 @@
+const numero = document.querySelector('.numero')
+
+
+
+let promise = new Promise((resolve,reject)=>{
+    let resultado= true
+    let tempo = 3000
+    setTimeout(()=>{
+        if(resultado){
+        resolve('deu tudo certo')
+        }else{
+            reject('deu tudo errado')
+        }
+    },tempo)
+
+})
+
+promise.then((retorno)=>{
+    numero.innerHTML = retorno
+    numero.classList.remove('erro')
+    numero.classList.add('ok')
+})
+
+promise.catch((retorno)=>{
+    numero.innerHTML = retorno
+    numero.classList.add('erro')
+    numero.classList.remove('ok')
+})
+
+numero.innerHTML = 'processando...'
+
+
+
