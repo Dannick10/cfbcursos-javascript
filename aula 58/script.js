4b11c4791730288ffea7c3db7add6c53e338c12a
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-12-18 14:15:09 -0300

    propriedades Math

---
+++ aula 58/script.js
@@ -0,0 +1,18 @@
+const olho = [...document.querySelectorAll('.olho')]
+
+let posx_mouse
+let posy_mouse
+
+window.addEventListener('mousemove',(evt)=>{
+    posx_mouse =evt.clientX
+    posy_mouse= evt.clientY
+
+    const rot=Math.atan2(posy_mouse,posx_mouse) *360 / Math.PI 
+
+
+    olho.forEach((o)=> {
+        o.style.transform='rotate('+rot+'deg)'
+    })
+
+})
+
