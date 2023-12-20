4bb78a82ef6c92b8d9f9d37818b06bc81fc6c3a7
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-12-20 16:02:51 -0300

    location url e historico

---
+++ aula 61/script.js
@@ -0,0 +1,16 @@
+const url = document.querySelector('#url')
+const btn_url = document.querySelector('#btn_url')
+
+btn_url.addEventListener('click',(evt)=>{
+    //window.location = 'https//www.google.com.br'
+    //window.location.replace('www.google.com.br')
+   // window.location.assign('www.google.com.br')
+   // windown.location.reload()
+  // window.history.forward()
+  //window.history.go(1)
+    console.log(window.history.length)
+    console.log(url.value)
+    window.location = url.value
+   
+})
+
