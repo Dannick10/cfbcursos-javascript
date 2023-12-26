0d6bfa8c27c4aafdaebdd5698d10892ff936ed1a
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-12-26 15:03:36 -0300

    modulo

---
+++ aula 66/script.js
@@ -0,0 +1,10 @@
+import {cursos} from './script2.js'
+import { getCursos } from './script2.js'
+import { getTodosCursos } from './script2.js'
+
+
+const div = document.querySelector('#objetos')
+
+console.log(cursos)
+console.log(getCursos[0])
+console.log(getTodosCursos())
---
+++ aula 66/script2.js
@@ -0,0 +1,21 @@
+const cursos=['java-script','css','html','Arduino','Raspberry','c++']
+const carros=['Polo','t-cross','golf','onix','cruze','argo']
+
+
+
+
+/*const getTodosCursos=()=>{
+    return cursos
+}*/
+
+export default function getTodosCursos(){
+    return cursos
+}
+
+function getCursos(){
+    return cursos
+}
+
+const div = document.querySelector('#objetos')
+
+export {cursos,getCursos} 
