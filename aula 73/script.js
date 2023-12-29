44589d352a56d861d9d2a67975246a75db7c9402
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-12-29 14:00:40 -0300

    string manipulaçao

---
+++ aula 73/script.js
@@ -0,0 +1,31 @@
+let nome=new String('Daniel Rocha')
+let nome2=new String('Daniel Rocha')
+
+console.log(nome)
+console.log(typeof(nome))
+
+console.log(nome.charAt(0))
+console.log(nome.charAt(1))
+
+console.log(nome.charCodeAt(0))
+
+console.log(nome.concat(' rocha'))
+
+console.log(nome.indexOf('D'))
+
+
+console.log(nome.localeCompare(nome2))
+
+console.log(nome.replace('a','p'))
+
+console.log(nome.search('Rocha'))
+
+console.log(nome.slice(7))
+console.log(nome.slice(0,7))
+
+let arr_nome=nome.split(' ')
+
+console.log(arr_nome)
+
+
+
