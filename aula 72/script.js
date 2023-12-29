ceed22fc0e406e5d009817f65d927f7bca530fa6
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2023-12-29 13:44:31 -0300

    string

---
+++ aula 72/script.js
@@ -0,0 +1,15 @@
+let nome=new String('Daniel')
+
+console.log(nome)
+console.log(typeof(nome))
+
+console.log(nome.charAt(0))
+console.log(nome.charAt(1))
+
+console.log(nome.charCodeAt(0))
+
+console.log(nome.concat(' rocha'))
+
+console.log(nome.indexOf('D'))
+
+console.log(nome.lastIndexOf('n'))
