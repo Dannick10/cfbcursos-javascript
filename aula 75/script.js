b61ec782e394499c258893cc3e84b93872e7cd40
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-01-02 14:36:23 -0300

    Regex

---
+++ aula 75/script.js
@@ -0,0 +1,12 @@
+let nome = new String('Daniel Rocha Silva')
+
+console.log(nome)
+
+let nome_incluso = nome.match(/a/g )
+console.log(nome_incluso)
+
+
+console.log(nome.search(/rocha/i))
+
+console.log(nome.match(/a/ig))
+
