0f898c36403190f5b62ba2d99c13411a1c78701e
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-01-02 15:04:12 -0300

    regezx

--- aula 75/script.js
+++ aula 75/script.js
@@ -1,12 +1,26 @@
-let nome = new String('Daniel Rocha Silva')
+let nome = new String('Daniel Rocha Silva 1999')
+let email='Daniel@hotmail.com'
+let numeros='1,10,100,1000'
 
 console.log(nome)
-
-let nome_incluso = nome.match(/a/g )
-console.log(nome_incluso)
-
 
 console.log(nome.search(/rocha/i))
 
 console.log(nome.match(/a/ig))
+
+console.log(nome.match(/[a-l|0-9]/ig))
+
+
+//meta caracteres
+console.log(nome.match(/\d/ig)) //numeros
+console.log(nome.match(/\s/ig)) //espaçõs
+console.log(nome.match(/\bD/ig)) //
+
+//quantificadores
+
+console.log(nome.match(/a+|c+ /ig))
+console.log(nome.match(/an*/ig))
+console.log(numeros.match(/10?/ig))
+console.log(numeros.match(/10+/ig))
+console.log(numeros.match(/10*/ig))
 
