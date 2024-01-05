f1d6ce78aa7e81d92490de6d91d57834551ab629
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-01-05 15:35:28 -0300

    acesskey

---
+++ aula 81/script.js
@@ -0,0 +1,32 @@
+const q1 = document.querySelector('#q1')
+const q2 = document.querySelector('#q2')
+const posx = document.querySelector('#posx')
+const posy = document.querySelector('#posy')
+const largura = document.querySelector('#largura')
+const altura = document.querySelector('#altura')
+
+
+q1.acessKey='b'
+q2.acessKey='n'
+
+console.log('q1' + q1.acessKey)
+console.log('q2' + q2.acessKey)
+
+
+const info=(el)=>{  
+  let DOMRect_q = el.getBoundingClientRect()
+  posx.innerHTML = `posx:${DOMRect_q.x}`
+  posy.innerHTML = `posy:${DOMRect_q.y}`
+  largura.innerHTML = `largura:${DOMRect_q.width}`
+  altura.innerHTML = `altura:${DOMRect_q.height}`
+}
+
+
+q1.addEventListener('click',(evt)=>{
+  info(evt.target)
+  
+})
+
+q2.addEventListener('click',(evt)=>{
+  info(evt.target)
+})
