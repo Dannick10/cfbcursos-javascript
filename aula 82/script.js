8b22082fc9a396841e1c0f1e02ae238e6dd38b37
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-01-05 16:08:40 -0300

    destruturação

---
+++ aula 82/script.js
@@ -0,0 +1,20 @@
+
+/*
+let a,b,c,d
+[{a,b,c,d}={a:'verde',b:'azul',c:'branco',d:'amarelo'}]
+
+*/
+
+let numeros =()=>{
+  return [10,20,30,40]
+}
+
+console.log(numeros())
+
+
+let [a,b,c,d]=numeros()
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
