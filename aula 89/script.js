7273a66a670d5cd6119f66638ba0dbae50d1cce4
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-01-08 16:01:29 -0300

    api

---
+++ aula 89/script.js
@@ -0,0 +1,17 @@
+
+
+
+const endpoint = 'https://43955f34-3630-43b6-a919-feb552573a60-00-34bxkhtriukjh.janeway.replit.dev'
+
+
+const api_dados=()=>{
+fetch(endpoint)
+
+.then(res=>res.json())
+.then(dados=>{
+    console.log(dados)
+   
+})
+}
+
+setInterval(api_dados,3000)
